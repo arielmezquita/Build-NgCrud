a320e8af915ed05f70b90b03e4fd835dda14ca67
Ariel Mezquita <joseariel.rey@gmail.com>
2021-02-01 23:10:18 -0400

    Firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyApKg7q9WSOU2oSsUm2zUygPivQU3aNhFY",
+    authDomain: "ngbuildcrud.firebaseapp.com",
+    projectId: "ngbuildcrud",
+    storageBucket: "ngbuildcrud.appspot.com",
+    messagingSenderId: "461141209910",
+    appId: "1:461141209910:web:af2b5899b5cc698677b997"
+  }
 };
